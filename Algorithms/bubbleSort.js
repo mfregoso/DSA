4fa282dd6adffe58d7f3944d37412664283b2515
mfregoso <mfregoso@users.noreply.github.com>
2018-11-16 23:19:16 -0800

    bubble sort improvement

--- Algorithms/bubbleSort.js
+++ Algorithms/bubbleSort.js
@@ -1,18 +1,22 @@
 const bubbleSort = arr => {
   let swapped = true;
+  let end = arr.length;
 
   const swap = i => {
     let current = arr[i];
     arr[i] = arr[i + 1];
     arr[i + 1] = current;
-    swapped = true;
   };
 
   while (swapped) {
     swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) swap(i);
+    for (let i = 0; i < end - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(i);
+        swapped = true;
+      }
     }
+    if (swapped) end--;
   }
   return arr;
 };
