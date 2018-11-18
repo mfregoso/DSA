0c9182fe4ac42b9bd58b4c9e74b5c022437edeef
mfregoso <mfregoso@users.noreply.github.com>
2018-11-17 16:42:58 -0800

    diy insertion sort

---
+++ Algorithms/insertionSort.js
@@ -0,0 +1,19 @@
+const iSort = arr => {
+  if (!arr) return undefined;
+
+  const swap = (i1, i2) => {
+    let temp = arr[i1];
+    arr[i1] = arr[i2];
+    arr[i2] = temp;
+  };
+
+  for (let chk = 1; chk < arr.length; chk++) {
+    for (let i = 0; i < chk; i++) {
+      if (arr[i] > arr[chk]) {
+        swap(i, chk);
+      }
+    }
+  }
+
+  return arr;
+};
