6e8f0dde28330a20a8d36272410788b0effd2066
mfregoso <mfregoso@users.noreply.github.com>
2018-11-20 21:54:36 -0800

    palindrome permutation rename

--- Algorithms/challenges/isPermutationOfAPalindrome.js
+++ Algorithms/challenges/isPermutationOfAPalindrome.js
@@ -4,12 +4,12 @@ const isPP = str => {
     table[s] ? table[s]++ : (table[s] = 1);
   }
 
-  let singleCount = 0;
+  let oddCount = 0;
   for (let s of str) {
-    if (table[s] % 2 !== 0) singleCount++;
+    if (table[s] % 2 !== 0) oddCount++;
   }
 
-  if (singleCount === 1 || singleCount === 0) {
+  if (oddCount === 1 || oddCount === 0) {
     return true;
   } else {
     return false;
