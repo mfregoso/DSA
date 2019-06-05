338b8717a30f1960e00072c61cb7be12742da265
mfregoso <mfregoso@users.noreply.github.com>
2019-06-04 23:45:03 -0700

    improve formatting

--- Algorithms/challenges/fibonaccis.js
+++ Algorithms/challenges/fibonaccis.js
@@ -5,9 +5,11 @@ const fibonacci = n => {
   const fibs = new Array(n + 1);
   fibs[0] = 0;
   fibs[1] = 1;
+
   for (let i = 2; i <= n; i++) {
     fibs[i] = fibs[i - 1] + fibs[i - 2];
   }
+
   return fibs[n];
 };
 
@@ -15,10 +17,24 @@ const fibonacci = n => {
 const fibo = n => {
   const fibs = [0, 1];
   if (n < 2) return fibs[n];
+
   for (let i = 2; i <= n; i++) {
       const next = fibs[0] + fibs[1];
       fibs[0] = fibs[1];
       fibs[1] = next;
   }
+  
   return fibs[1];
 }
+
+// recursive
+const fib = n => {
+  if (n < 1) return 0;
+  if (n === 1) return 1;
+
+  return fib(n-1) + fib(n-2);
+};
+
+console.log(fib(5));
+console.log(fibo(8));
+console.log(fibonacci(13));
--- Algorithms/challenges/isArr2TheSquareOfArr1.js
+++ Algorithms/challenges/isArr2TheSquareOfArr1.js
@@ -2,21 +2,21 @@ const same = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   }
+
   const index = {};
-  let same = true;
+  let isSquare = true;
+
   for (let nbr of arr1) {
     let num = nbr ** 2;
     index[num] ? index[num]++ : (index[num] = 1);
   }
+
   for (let sq of arr2) {
-    index[sq] > 0 ? index[sq]-- : (same = false);
-    // if (index[sq] > 0) {
-    //  index[sq]--;
-    // } else {
-    //   same = false;
-    // }
+    index[sq] > 0 ? index[sq]-- : (isSquare = false);
   }
-  return same;
+
+  return isSquare;
 };
 
-console.log(same([2, 3, 3], [9, 4, 4]));
+console.log(same([2, 3, 3], [9, 4, 4])); // false
+console.log(same([2, 3, 3], [9, 9, 4])); // true
