d7223f2c3ea50ebaa7d52101ad557e01c22a23b8
mfregoso <mfregoso@users.noreply.github.com>
2019-06-05 23:35:50 -0700

    fix in-place de-duplication

--- Algorithms/challenges/remove_duplicates_InPlaceSplice.js
+++ Algorithms/challenges/remove_duplicates_InPlaceSplice.js
@@ -1,12 +1,15 @@
 const deDupe = arr => {
   const table = {};
+
   for (let i = 0; i < arr.length; i++) {
     if (table[arr[i]]) {
-      arr.splice(i, 1);
-      i = i - 1;
+      arr.splice(i--, 1);
     }
-    table[arr[i]] = i;
+    table[arr[i]] = true;
   }
+
   return arr;
 };
-console.log(deDupe([2, 3, 3, 4, 6, 10, 6, 14]));
+
+console.log(deDupe([2, 3, 3, 4, 6, 10, 6, 14])); // [ 2, 3, 4, 6, 10, 14 ]
+console.log(deDupe([2, 2, 3, 3, 2])); // [2, 3]
