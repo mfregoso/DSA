21eccebcbf027d45875038701984edde58c83068
mfregoso <mfregoso@users.noreply.github.com>
2019-06-06 23:46:24 -0700

    fix falsy check bug due to using array index

--- Algorithms/challenges/remove_duplicates.js
+++ Algorithms/challenges/remove_duplicates.js
@@ -1,12 +1,15 @@
 const deDupe = arr => {
   const table = {};
   const unique = [];
+  
   for (let i = 0; i < arr.length; i++) {
     if (!table[arr[i]]) {
       unique.push(arr[i]);
     }
-    table[arr[i]] = i;
+    table[arr[i]] = true;
   }
   return unique;
 };
-console.log(deDupe([2, 3, 3, 4, 6, 10, 6, 14]));
+
+console.log(deDupe([2, 3, 3, 4, 6, 10, 6, 14])); // [ 2, 3, 4, 6, 10, 14 ]
+console.log(deDupe([2, 2, 3, 3, 2])); // [2, 3]
