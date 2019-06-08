431fa3a663ea40d814d8870f29305213d9393327
mfregoso <mfregoso@users.noreply.github.com>
2019-06-07 22:41:46 -0700

    simplify frequency check with only one table

--- Algorithms/challenges/sameFrequencyOfDigits.js
+++ Algorithms/challenges/sameFrequencyOfDigits.js
@@ -1,24 +1,27 @@
 function sameFrequency(int1, int2) {
   const nums1 = int1.toString().split("");
   const nums2 = int2.toString().split("");
-  if (nums1.length !== nums2.length) {
-    return false;
-  }
+  if (nums1.length !== nums2.length) return false;
+  
   const count1 = {};
-  const count2 = {};
+
   for (let num of nums1) {
     count1[num] ? count1[num]++ : (count1[num] = 1);
   }
+
   for (let num of nums2) {
-    count2[num] ? count2[num]++ : (count2[num] = 1);
-  }
-  const keys = Object.keys(count1);
-  for (let num in keys) {
-    if (count1[num] !== count2[num]) {
+    if (count1[num] > 0) {
+      count1[num]--;
+    } else {
       return false;
     }
   }
+
   return true;
 }
 
-console.log(sameFrequency(182, 281));
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(182, 2811));// false
+console.log(sameFrequency(2, 2));     // true
+console.log(sameFrequency(22, 20));   // false
+console.log(sameFrequency(200, 201)); // false
