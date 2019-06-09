b65ef3af0138b2d485b0db8b9f8ad6d227828673
mfregoso <mfregoso@users.noreply.github.com>
2019-06-08 20:16:14 -0700

    reformat, add examples

--- Algorithms/challenges/isPermutationOfAPalindrome.js
+++ Algorithms/challenges/isPermutationOfAPalindrome.js
@@ -1,18 +1,23 @@
 const isPP = str => {
-  let table = {};
+  const table = {};
+
   for (let s of str) {
     table[s] ? table[s]++ : (table[s] = 1);
   }
 
   let oddCount = 0;
-  let keys = Object.keys(table);
-  for (let s of keys) {
-    if (table[s] % 2 !== 0) oddCount++;
+  const keyCounts = Object.values(table);
+
+  for (let keyCount of keyCounts) {
+    if (keyCount % 2 !== 0) oddCount++;
   }
 
-  if (oddCount === 1 || oddCount === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return oddCount <= 1 ? true : false;
 };
+
+console.log(isPP("box")); // false
+console.log(isPP("caracer")); // true
+console.log(isPP("racecar")); // true
+console.log(isPP("bbbbo")); // true
+console.log(isPP("bobb")); // false
+console.log(isPP("bobbo")); // true
