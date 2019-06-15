464cc6d2f93449a435689fd5098e573a11f15848
mfregoso <mfregoso@users.noreply.github.com>
2019-06-14 23:11:03 -0700

    refactor rot13 to support both encode and decode

--- Algorithms/challenges/rot13CaesarsCipher.js
+++ Algorithms/challenges/rot13CaesarsCipher.js
@@ -1,20 +1,26 @@
-const rot13 = str => {
-  const decoded = [];
+const rot13 = (str, decoding) => {
+  const solution = [];
 
   const convert = s => {
     const upperCase = /[A-Z]/.test(s);
-    const limit = upperCase ? 64 : 96;
-    const code = s.charCodeAt(0) - 13;
-    return String.fromCharCode(code <= limit ? code + 26 : code);
+    const limit = upperCase ? decoding ? 64: 91 : decoding ? 96 : 122;
+    const code = s.charCodeAt(0) + (decoding ? -13 : 13);
+    if (decoding) return String.fromCharCode(code <= limit ? code + 26 : code);
+    return String.fromCharCode(code >= limit ? code - 26 : code);
+    
   }
 
   for (let s of str) {
-    decoded.push(/[A-Z,a-z]/.test(s) ? convert(s) : s);
+    solution.push(/[A-Z,a-z]/.test(s) ? convert(s) : s);
   }
 
-  return decoded.join("");
+  return solution.join("");
 }
 
-console.log(rot13("SERR CVMMN!")); // FREE PIZZA!
-console.log(rot13("#serr cvMMN??")); // #free piZZA??
-console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+const encode = (str) => rot13(str);
+const decode = (str) => rot13(str, true);
+
+const encoded = encode("FREE PIZZA!");
+console.log(decode(encoded)); // FREE PIZZA!
+console.log(decode("#serr cvMMN??")); // #free piZZA??
+console.log(decode("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
